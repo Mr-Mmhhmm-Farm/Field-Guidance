d31cab5f1a4d3042e64a5d909e4cc5836bfa0158
Andrew Carbert <ac.believer@gmail.com>
2023-05-04 23:32:48 -0600

    gps

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ $(document).ready(function() {
     window.addEventListener('deviceorientation', function( event ) {
         deviceOrientationData = event;
         console.log(event);
-        $("#debug").html("x: " + event.beta + ",</br>y: " + event.gamma + ",</br>z: " + event.alpha + ",</br>ts: " + event.timeStamp);
+        $("#debug-gyro").html("x: " + event.beta + ",</br>y: " + event.gamma + ",</br>z: " + event.alpha + ",</br>ts: " + event.timeStamp);
         $("#heading").css("transform", "rotate("+event.gamma+"deg)");
     }, false);
+    navigator.geolocation.watchPosition(showPosition);
+
 });
+
+function showPosition(position){
+    $("#debug-gps").html("Latitude: " + position.coords.latitude + "</br>Longitude: " + position.coords.longitude);
+}
