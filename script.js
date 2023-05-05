0e044ddc9600ff4bbdd4ff5b58e4d495bc885136
Andrew Carbert <ac.believer@gmail.com>
2023-05-04 23:08:49 -0600

    update

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 var zoom = 1;
 
+$(document).ready(function() {
+    window.addEventListener('deviceorientation', function( event ) {
+        deviceOrientationData = event;
+        console.log(event);
+        $("#debug").text("x: " + event.beta + ", y: " + event.gamma + ", z: " + event.alpha + ", ts: " + event.timeStamp);
+    }, false);
+});
